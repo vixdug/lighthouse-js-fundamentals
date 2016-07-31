12499d1f822f0b9c439a3bb94e679dc31bee754d
Victoria <victoria.duggan@shopify.com>
2016-07-31 08:13:14 +0000

    update array

---
+++ icecream.js
@@ -0,0 +1,10 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
