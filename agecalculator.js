690a5aed7a741183afd846adc6a293c650e544d3
Victoria <victoria.duggan@shopify.com>
2016-07-31 04:38:34 +0000

    age calculations

---
+++ agecalculator.js
@@ -0,0 +1,9 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  return(name + " is " + (currentYear - yearOfBirth) + " years old.");
+
+}
+
+
+console.log(ageCalculator("Suzie", 1984, 2016));
+console.log(ageCalculator("Ali", 2015, 2015));
+console.log(ageCalculator("Jack", 2004, 2016));
